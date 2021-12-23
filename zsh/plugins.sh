c1870de3784e59f136be9d84c8aabcbad10dc904
Kesse Jones <kjonesfc@outlook.com>
2021-12-23 14:35:02 -0300

    added vi mode plugin to zsh

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PATH_INSTALL=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-PLUGINS="zsh-autosuggestions zsh-syntax-highlighting"
+PLUGINS="zsh-users/zsh-autosuggestions zsh-users/zsh-syntax-highlighting jeffreytse/zsh-vi-mode"
 
 for plugin in $PLUGINS; do
     echo "Install plugin: $plugin"
-    git clone https://github.com/zsh-users/${plugin}.git ${PATH_INSTALL}/plugins/${plugin}
+    git clone https://github.com/${plugin}.git ${PATH_INSTALL}/plugins/${plugin}
 done
 
 echo "Done!"
