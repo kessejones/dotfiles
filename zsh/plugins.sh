dcbec919caad4b5ac2bcfa72c0c11ca9ebb27752
Kesse Jones <kjonesfc@outlook.com>
2021-12-23 20:04:14 -0300

    install plugins

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 PATH_INSTALL=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-PLUGINS="zsh-users/zsh-autosuggestions zsh-users/zsh-syntax-highlighting jeffreytse/zsh-vi-mode"
+PLUGINS="zsh-users/zsh-autosuggestions zsh-users/zsh-syntax-highlighting softmoth/zsh-vim-mode"
+
+plugin_name(){
+    splitString=$1
+    result="$(echo $splitString | cut -d"/" -f2)"
+    echo $result
+}
 
 for plugin in $PLUGINS; do
     echo "Install plugin: $plugin"
-    git clone https://github.com/${plugin}.git ${PATH_INSTALL}/plugins/${plugin}
+    git clone https://github.com/${plugin}.git ${PATH_INSTALL}/plugins/$(plugin_name $plugin)
 done
 
 echo "Done!"
