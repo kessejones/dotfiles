a329de462dc5ed7ee6acc14a512e3e9d32461093
Kesse Jones <kjonesfc@outlook.com>
2022-01-23 12:24:24 -0300

    (fish) added binding to switch to normal mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,6 +23,7 @@ set -g fish_key_bindings fish_vi_key_bindings
 
 bind -M insert \[aa accept-autosuggestion
 bind -M normal \[aa accept-autosuggestion
+bind -M insert \cj 'set fish_bind_mode default; commandline -f repaint'
 
 fish_add_path ~/.local/bin
 
