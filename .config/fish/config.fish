1690df05a8c40fc49be25fab9df5a2427e22b1de
Kesse Jones <kjonesfc@outlook.com>
2022-03-28 20:21:52 -0300

    (fish) organize key mapping

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,18 @@
 # general
 set -gx EDITOR nvim
 
+# disable welcome message
 set fish_greeting
 
-# theme
-#set -g theme_color_scheme terminal-dark
-#set -g fish_prompt_pwd_dir_length 1
-#set -g theme_display_user yes
-#set -g theme_hide_hostname yes
-#set -g theme_hostname no 
+# custom colors
 set fish_color_command green
 set fish_color_autosuggestion 656565 
 set fish_color_error red
 
-# aliases
-alias g git
-alias v nvim
-alias t tmux
-
-set -g fish_key_bindings fish_vi_key_bindings
-
-bind -M insert \[aa accept-autosuggestion
-bind -M normal \[aa accept-autosuggestion
-bind -M insert \cj 'set fish_bind_mode default; commandline -f repaint'
-
+# path
 fish_add_path ~/.local/bin
 
+# local config
 if test -f ~/.user-config.fish
     source ~/.user-config.fish
 end
