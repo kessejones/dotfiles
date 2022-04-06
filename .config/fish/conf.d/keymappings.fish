eb0bc4a74c9d0b60c5089d9f8623de7a5560e77a
Kesse Jones <kjonesfc@outlook.com>
2022-04-06 11:55:09 -0300

    (fish) added git status bind

--- .config/fish/conf.d/keymappings.fish
+++ .config/fish/conf.d/keymappings.fish
@@ -3,6 +3,7 @@ set -g fish_key_bindings fish_vi_key_bindings
 bind -M insert \[aa accept-autosuggestion
 bind -M normal \[aa accept-autosuggestion
 
+bind -M insert \cs _fzf_search_git_status
 bind -M insert \cg _fzf_search_git_log
 bind -M insert \cf _fzf_search_directory
 
