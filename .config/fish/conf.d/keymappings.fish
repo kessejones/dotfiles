ddade3b45f42c7812878f9b7cca29c1aa3912d78
Kesse Jones <kjonesfc@outlook.com>
2022-04-06 21:09:47 -0300

    (fish) added bind ctrl+space run autosuggestion

--- .config/fish/conf.d/keymappings.fish
+++ .config/fish/conf.d/keymappings.fish
@@ -1,10 +1,11 @@
+fish_user_key_bindings
+
 set -g fish_key_bindings fish_vi_key_bindings
 
-bind -M insert \[aa accept-autosuggestion
-bind -M normal \[aa accept-autosuggestion
+bind -M insert \cj _normal_mode
+bind -M insert 'fj' _normal_mode
 
 bind -M insert \cs _fzf_search_git_status
 bind -M insert \cg _fzf_search_git_log
 bind -M insert \cf _fzf_search_directory
-
-bind -M insert \cj 'set fish_bind_mode default; commandline -f repaint'
+bind -M insert \ce _fzf_change_directory
---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,3 @@
+function fish_user_key_bindings
+    bind --preset -M insert -k nul accept-autosuggestion
+end
