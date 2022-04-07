d85efa2623d616eccb90d5038e8c277b3822938f
Kesse Jones <kjonesfc@outlook.com>
2022-04-07 08:42:01 -0300

    (fish) added search _fzf_change_directory_home

--- .config/fish/conf.d/keymappings.fish
+++ .config/fish/conf.d/keymappings.fish
@@ -3,9 +3,9 @@ fish_user_key_bindings
 set -g fish_key_bindings fish_vi_key_bindings
 
 bind -M insert \cj _normal_mode
-bind -M insert 'fj' _normal_mode
 
 bind -M insert \cs _fzf_search_git_status
 bind -M insert \cg _fzf_search_git_log
 bind -M insert \cf _fzf_search_directory
 bind -M insert \ce _fzf_change_directory
+bind -M insert \ch _fzf_change_directory_home
