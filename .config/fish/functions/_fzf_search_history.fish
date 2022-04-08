ee2ea7fed203ba8ecc24a89f80fd65690bcbd647
Kesse Jones <kjonesfc@outlook.com>
2022-04-08 19:45:38 -0300

    (fish) added bind to _fzf_search_history

--- .config/fish/functions/_fzf_search_history.fish
+++ .config/fish/functions/_fzf_search_history.fish
@@ -8,16 +8,26 @@ function _fzf_search_history --description "Search command history. Replace the 
         _fzf_wrapper --read0 \
             --tiebreak=index \
             --query=(commandline) \
+            --header-first \
+            --header 'Search in history' \
             # preview current command using fish_ident in a window at the bottom 3 lines tall
             # --preview="echo -- {4..} | fish_indent --ansi" \
             # --preview-window="bottom:3:wrap" \
+            --bind "ctrl-space:execute(echo 'REPLACE │ {}')+abort,enter:execute(echo 'RUN │ {}')+abort" \
             $fzf_history_opts |
         string collect
     )
 
     if test $status -eq 0
-        set command_selected (string split --max 1 " │ " $command_with_ts)[2]
-        commandline --replace -- $command_selected
+        set selected_parts (string split --max 3 " │ " $command_with_ts)
+
+        set command_selected $selected_parts[3]
+        switch $selected_parts[1]
+            case RUN
+                eval $command_selected
+            case REPLACE
+                commandline --replace -- $command_selected
+        end 
     end
 
     commandline --function repaint
