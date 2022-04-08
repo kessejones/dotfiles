db36e92df8e0eccd7906b341bf698f091885c72c
Kesse Jones <kjonesfc@outlook.com>
2022-04-08 08:16:34 -0300

    (fish) added header in _fzf_search_directory and _fzf_change_directory_home

--- .config/fish/functions/_fzf_change_directory_home.fish
+++ .config/fish/functions/_fzf_change_directory_home.fish
@@ -4,7 +4,9 @@ function _fzf_change_directory_home --description "Search the current directory.
     set fzf_arguments --multi --ansi $fzf_dir_opts
 
     set --append fd_opts --base-directory=$HOME
-    set file_paths_selected (fd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
+
+    set --append fzf_arguments --header-first --header "Find directory ($HOME)"
+    set file_paths_selected (fd $fd_opts 2>/dev/null | sort | _fzf_wrapper $fzf_arguments)
 
     if test $status -eq 0
         cd $HOME/$file_paths_selected
