59de0f902703c37e97e41bf97e435bc6438e92a3
Kesse Jones <kjonesfc@outlook.com>
2022-04-23 09:44:55 -0300

    (fish) move keymappings to fish_user_key_bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,17 @@
 function fish_user_key_bindings
+    bind -M insert \cj _normal_mode
+
+    bind -M insert \cs _fzf_search_git_status
+    bind -M insert \cg _fzf_search_git_log
+    bind -M insert \cf _fzf_search_directory
+    bind -M insert \ce _fzf_change_directory
+    bind -M insert \ch _fzf_change_directory_home
+
+    bind -M insert \cb '_bookmark -m list'
+    bind -M insert \ca '_bookmark -m save'
+    bind -M insert \cx '_bookmark -m remove'
+
     bind --preset -M insert -k nul accept-autosuggestion
+
+    set -g fish_key_bindings fish_vi_key_bindings
 end
