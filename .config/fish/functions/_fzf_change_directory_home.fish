bd45492c769077e65c29adc02ebd4dfc7e897ba9
Kesse Jones <kjonesfc@outlook.com>
2022-04-26 20:18:16 -0300

    (fish) ignore macos folders on _fzf_change_directory_home

--- .config/fish/functions/_fzf_change_directory_home.fish
+++ .config/fish/functions/_fzf_change_directory_home.fish
@@ -6,6 +6,13 @@ function _fzf_change_directory_home --description "Search the current directory.
     set --append fd_opts --base-directory=$HOME
 
     set --append fzf_arguments --header-first --header "Find directory ($HOME)"
+
+    set --append fd_opts --exclude Applications/
+    set --append fd_opts --exclude Library/
+    set --append fd_opts --exclude Movies/
+    set --append fd_opts --exclude Music/
+    set --append fd_opts --exclude node_modules/
+
     set file_paths_selected (fd $fd_opts 2>/dev/null | _fzf_wrapper $fzf_arguments)
 
     if test $status -eq 0
