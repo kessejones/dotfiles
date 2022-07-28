8aee57fefcfcc5cc9946ee1fc89b5e9e71f66e5d
Kesse Jones <kjonesfc@outlook.com>
2022-07-27 21:18:28 -0300

    (scripts) add scripts for install apps

---
+++ .scripts/apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APPS= \
+    neovim \
+    fzf \
+    fish \
+    tmux \
+    ripgrep \
+    tree-sitter \
+
+
+brew install $APPS
---
+++ .scripts/fisher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+
+fisher install jorgebucaran/nvm.fish
