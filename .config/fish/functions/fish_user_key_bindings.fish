1cfaa4c9d21465a91e8e2169a170140f72affe10
Kesse Jones <kjonesfc@outlook.com>
2022-09-23 09:54:06 -0300

    (fish) added binding to accept autocomplete

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,6 @@ function fish_user_key_bindings
     bind -M insert \cj _normal_mode
 
     bind -M insert \cs _fzf_search_git_status
-    bind -M insert \cg _fzf_search_git_log
     bind -M insert \cf _fzf_search_directory
     bind -M insert \ce _fzf_change_directory
     bind -M insert \ch _fzf_change_directory_home
@@ -11,6 +10,8 @@ function fish_user_key_bindings
     bind -M insert \ca '_bookmark -m save'
     bind -M insert \cx '_bookmark -m remove'
 
+    bind --user -M insert \cg accept-autosuggestion
+    bind --preset -M insert \cu accept-autosuggestion
     bind --preset -M insert -k nul accept-autosuggestion
 
     set -g fish_key_bindings fish_vi_key_bindings
