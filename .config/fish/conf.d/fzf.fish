e6790902b74d1c5d7da6286e05fcdb545828d376
Kesse Jones <kjonesfc@outlook.com>
2022-10-14 21:56:11 -0300

    chore: set layout reverse to fzf

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -12,7 +12,9 @@ set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub)
 set -Ux FZF_DEFAULT_OPTS "\
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--layout=reverse \
+--border"
 
 # Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
 fzf_configure_bindings
