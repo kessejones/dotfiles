992cced920d52233ed2c0bc1ae0b1fe006bad9d0
Kesse Jones <kjonesfc@outlook.com>
2022-11-16 00:42:39 -0300

    feat(fish): added binding to select dotfile project

---
+++ .config/fish/functions/dotconf.fish
@@ -0,0 +1,20 @@
+function _build_dots
+    echo "dotfiles $HOME/.dotfiles"
+    echo "nvim $HOME/.config/nvim"
+    echo "awesome $HOME/.config/awesome"
+    echo "fish $HOME/.config/fish"
+    echo "yabai $HOME/.config/yabai"
+    echo "skhd $HOME/.config/skhd"
+    echo "alacritty $HOME/.config/alacritty"
+end
+
+function dotconf
+    set -l dots (_build_dots)
+
+    clear
+    set -l selected (gum choose --limit 1 $dots | cut -d' ' -f2)
+    if test -n "$selected"
+        cd $selected
+        commandline --function repaint
+    end
+end
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -9,6 +9,7 @@ function fish_user_key_bindings
     bind -M insert \cb '_bookmarks -m list'
     bind -M insert \ca '_bookmarks -m save'
     bind -M insert \cx '_bookmarks -m remove'
+    bind -M insert \cy dotconf
 
     bind --user -M insert \cg accept-autosuggestion
     bind --preset -M insert \cu accept-autosuggestion
