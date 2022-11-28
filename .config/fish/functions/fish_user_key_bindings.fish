6c8c1f20ca1f371ec70072e97e406b477e386219
Kesse Jones <kjonesfc@outlook.com>
2022-11-28 16:39:31 -0300

    chore(fish): added new key binding

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,13 +1,11 @@
 function fish_user_key_bindings
     # bind -M insert \cj _normal_mode
 
-    bind --user -M insert \ce _fzf_change_directory
     bind --user -M insert \ch _fzf_change_directory_home
     bind --user -M insert \cb '_bookmarks -m list'
     bind --user -M insert \ca '_bookmarks -m save'
     bind --user -M insert \cx '_bookmarks -m remove'
-    bind --user -M insert \c_ dotconf
-    bind --user -M insert ^/ dotconf # for macos
+    bind --user -M insert \ce dotconf
     bind --user -M insert \cf accept-autosuggestion
     bind --user -M insert \cg accept-autosuggestion execute
 
