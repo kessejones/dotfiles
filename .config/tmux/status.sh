15f7f1453a40f9b157c17616376680ef33b411f9
Kesse Jones <kjonesfc@outlook.com>
2022-12-04 11:30:57 -0300

    chore(tmux): change tmux statusbar colors

--- .config/tmux/status.sh
+++ .config/tmux/status.sh
@@ -5,19 +5,19 @@ source ~/.config/tmux/colors.sh
 
 # panes
 set pane-border-style "fg=${blue}"
-set pane-active-border-style "bg=default fg=${blue}"
+set pane-active-border-style "bg=default,fg=${blue}"
 
 # status bar
-status_bg=${surface0}
+status_bg=${base}
 status_fg=${text}
 
 session_bg=${blue}
 session_fg=${mantle}
 
-win_bg=${surface1}
-win_fg=${mantle}
+win_bg=${surface0}
+win_fg=${overlay1}
 
-win_active_bg=${overlay2}
+win_active_bg=${overlay0}
 win_active_fg=${mantle}
 
 set status on
@@ -27,8 +27,7 @@ set status-fg ${status_fg}
 set window-status-style "fg=${win_fg},bg=${win_bg}"
 set window-status-current-style "fg=${win_active_fg},bg=${win_active_bg}"
 set window-status-separator ''
-set window-status-format "#[fg=${status_bg},nobold]#[fg=${win_fg},nobold] #{?window_start_flag,, }#I:#W#{?window_flags,#F, } #[fg=${win_bg},bg=${status_bg},nobold]"
-set window-status-current-format "#[fg=${status_bg},nobold] #[fg=${win_active_fg},nobold]#{?window_start_flag,, }#I:#W#{?window_flags,#F, } #[fg=${win_active_bg},bg=${status_bg},nobold]"
-set status-left "#[fg=${session_fg},bg=${session_bg},nobold] #S #[bg=${status_bg},fg=${session_bg},nobold]"
-set status-right "#[fg=${session_bg},bg=${status_bg},nobold]#[fg=${session_fg},bg=${session_bg},nobold] %H:%M  %d/%m/%y "
-
+set window-status-format "#[fg=${status_bg},nobold]#[fg=${win_fg},bold] #{?window_start_flag,, }#I:#W#{?window_flags,#F, } #[fg=${win_bg},bg=${status_bg},nobold]"
+set window-status-current-format "#[fg=${status_bg},nobold] #[fg=${win_active_fg},bold]#{?window_start_flag,, }#I:#W#{?window_flags,#F, } #[fg=${win_active_bg},bg=${status_bg},nobold]"
+set status-left "#[fg=${session_fg},bg=${session_bg},bold] #S #[bg=${status_bg},fg=${session_bg},nobold]"
+set status-right "#[fg=${session_bg},bg=${status_bg},bold]#[fg=${session_fg},bg=${session_bg},bold] %H:%M  %d/%m/%y "
