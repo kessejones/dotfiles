e5a43d30ccf1df5b21a897908eb1465d5d09e3a7
Kesse Jones <kjonesfc@outlook.com>
2022-12-18 17:23:14 -0300

    feat(awesome): added network applet for autostart

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -5,6 +5,7 @@ local programs = {
     os.getenv("HOME") .. "/.scripts/linux/monitor.sh",
     os.getenv("HOME") .. "/.scripts/linux/enable-tapping-touchpad.sh",
     os.getenv("HOME") .. "/.scripts/linux/polkit-gnome-authentication-agent.sh",
+    "nm-applet",
 }
 
 for _, program in ipairs(programs) do
