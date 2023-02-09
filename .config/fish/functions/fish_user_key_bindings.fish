e99587887406c1bc70c72d4d5f6021bf27311815
Kesse Jones <kjonesfc@outlook.com>
2023-02-09 10:35:42 -0300

    chore(fish): remove ls alias

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,7 +3,7 @@ function fish_user_key_bindings
 
     bind --user -M insert \ch _fzf_change_directory_home
     bind --user -M insert \cb '_bookmarks -m list'
-    bind --user -M insert \ca '_bookmarks -m save'
+    bind --user -M insert \cw '_bookmarks -m save'
     bind --user -M insert \cx '_bookmarks -m remove'
     bind --user -M insert \ce dotconf
     bind --user -M insert \cf accept-autosuggestion
