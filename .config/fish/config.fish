a08ebbc9abe36a38c484a40f02af35f54e42c6be
Kesse Jones <kjonesfc@outlook.com>
2023-02-12 20:27:58 -0300

    chore(fish): set fish cursors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,10 @@ export EDITOR=nvim
 # disable welcome message
 set fish_greeting
 
+set fish_cursor_insert line
+set fish_cursor_replace underscore
+set fish_cursor_replace_one underscore
+
 # local config
 if test -f ~/.config.fish
     source ~/.config.fish
