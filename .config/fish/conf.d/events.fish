bb75cbc131dfa46e5e9f82b0758795250bbd6e2b
Kesse Jones <kjonesfc@outlook.com>
2024-03-28 09:34:15 -0300

    feat(fish): added option to disable line separator

--- .config/fish/conf.d/events.fish
+++ .config/fish/conf.d/events.fish
@@ -1,5 +1,7 @@
 function prompt_separator --on-event fish_postexec
-    set_color $fish_color_surface1
-    echo (string repeat -n $COLUMNS '─')
-    echo -n $line
+    if test -e $fish_line_seperator 1
+        set_color $fish_color_surface1
+        echo (string repeat -n $COLUMNS '─')
+        echo -n $line
+    end
 end
