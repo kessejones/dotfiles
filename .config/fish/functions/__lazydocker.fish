0c4fbbf92b76c30c1ad95256740ab5a71d37100e
Kesse Jones <kjonesfc@outlook.com>
2024-04-01 09:47:57 -0300

    feat: added lazydocker with podman

--- .config/fish/functions/__lazydocker.fish
+++ .config/fish/functions/__lazydocker.fish
@@ -5,9 +5,16 @@ function __lazydocker --description "Run lazydocker with docker or podman"
         lazydocker
     else
         podman ps > /dev/null 2>&1
+
         if test $status -eq 0
+            set -l qemu_sock "$HOME/.local/share/containers/podman/machine/qemu/podman.sock"
+            set -l applehv_sock (podman machine inspect | jq --raw-output '.[0] | .ConnectionInfo.PodmanSocket.Path')
             # macos only
-            DOCKER_HOST=unix://$HOME/.local/share/containers/podman/machine/qemu/podman.sock lazydocker
+            if test -e $qemu_sock
+                DOCKER_HOST=unix://$qemu_sock lazydocker
+            else if test -e $applehv_sock
+                DOCKER_HOST=unix://$applehv_sock lazydocker
+            end
         else
             echo "Docker/Podman is not running"
         end
