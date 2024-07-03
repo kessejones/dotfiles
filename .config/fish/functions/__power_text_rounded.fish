2ab95ecafa1e7d02d26d0e5654338ca40e242203
Kesse Jones <kjonesfc@outlook.com>
2024-07-02 21:40:28 -0300

    chore: added fish config

---
+++ .config/fish/functions/__power_text_rounded.fish
@@ -0,0 +1,14 @@
+function __power_text_rounded
+    argparse 'b/background=' 'f/foreground=' 'r/reset=' 'o/bold' 't/text=' -- $argv
+        or return
+
+    set -l text "$_flag_t"
+    if test -n "$_flag_o"
+        set text (string join '' -- (set_color --bold $_flag_f) $text)
+    else
+        set text (string join '' -- (set_color $_flag_f) $text)
+    end
+
+    string join '' -- (set_color -b $_flag_r) (set_color $_flag_b) "" (set_color -b $_flag_b) "$text" $(set_color -b $_flag_r) (set_color $_flag_b) ""
+end
+
