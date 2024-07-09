8e2777278baca81c17473ab7d8c32ef3bc366ca6
Kesse Jones <kjonesfc@outlook.com>
2024-07-08 21:25:44 -0300

    chore(fish): forcing fish vi cursor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set fish_greeting
 set fish_cursor_insert line
 set fish_cursor_replace underscore
 set fish_cursor_replace_one underscore
+set fish_vi_force_cursor 1
 
 # local config
 if test -f ~/.config.fish
