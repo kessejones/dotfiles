b04ea14d8c9f52436b8cf7c1b7dbf72c0386eef0
Kesse Jones <kjonesfc@outlook.com>
2024-07-08 22:13:13 -0300

    chore: testing dotfiles in flake

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -1,11 +1,27 @@
 {
   outputs = {pkgs, ...}: {
     packages.x86_64-linux = {
+      kitty = pkgs.mkDerivation {
+        src = ../kitty;
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out
+          cp -r $src/* $out/
+
+          runHook postInstall
+        '';
+      };
+
       wezterm = pkgs.mkDerivation {
         src = ../wezterm;
         installPhase = ''
+          runHook preInstall
+
           mkdir -p $out
           cp -r $src/* $out/
+
+          runHook postInstall
         '';
       };
     };
