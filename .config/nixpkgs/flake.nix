acaa4d2440938686a17ee788963f71895c6b5c3d
Kesse Jones <kjonesfc@outlook.com>
2024-07-09 23:46:21 -0300

    chore: testing

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -1,11 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {flake-utils, ...}:
-    flake-utils.lib.eachDefaultSystem (system: {
-      nixosModules.default = import ./modules;
-    });
+  outputs = {...}: {
+    nixosModules.default = import ./modules;
+  };
 }
