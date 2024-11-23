661a3f015c19f8e11f57b127c0f0438a6d5405db
Kesse Jones <kjonesfc@outlook.com>
2024-11-23 14:04:39 -0300

    chore: testing

---
+++ .config/nixpkgs/modules/dotfiles/kitty.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.dotfiles.kitty;
+in {
+  options.dotfiles.kitty = {
+    enable = mkEnableOption "Fish dotfiles";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      kitty
+    ];
+
+    xdg.configFile."kitty/kitty.conf".enable = false;
+
+    home.file.".config/kitty/" = {
+      source = "${pkgs.dotfiles.kitty}/share/kitty";
+      recursive = true;
+    };
+  };
+}
