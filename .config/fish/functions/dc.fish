71c0a459c5b19b06875cf05cff6f22930d9c17b4
Kesse Jones <kjonesfc@outlook.com>
2024-12-16 09:21:40 -0300

    feat: added docker compose alias on fish shell

--- .config/fish/functions/dc.fish
+++ .config/fish/functions/dc.fish
@@ -1,3 +1,16 @@
-function dc --wraps "docker compose"
-    docker compose $argv
+function dc --wraps "docker-compose"
+    set -l args $argv[1..]
+
+    switch $args[1]
+        case 'upd'
+            set args 'up' '-d' $args[2..]
+        case 'lf'
+            set args 'logs' '-f' $args[2..]
+        case 'll'
+            set args 'logs' '-fn1' $args[2..]
+        case 'bb'
+            set args 'build' $args[2..]
+    end
+
+    docker compose $args
 end
