546d93a57c6f3fb69f9cc8d525fac964b666a142
Kesse Jones <kjonesfc@outlook.com>
2024-12-23 15:45:11 -0300

    chore(nixpkgs): added zellij config

---
+++ .config/nixpkgs/modules/dotfiles/zellij.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.dotfiles.zellij;
+in {
+  options.dotfiles.zellij = {
+    enable = mkEnableOption "Zellij dotfiles";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      zellij
+    ];
+
+    xdg.configFile."zellij/zellij.kdl".enable = false;
+    xdg.dataHome."zjstatus.wasm".source = "${pkgs.zjstatus}/share/zjstatus.wasm";
+
+    home.file.".config/zellij/" = {
+      source = "${pkgs.dotfiles.zellij}/";
+      recursive = true;
+    };
+  };
+}
