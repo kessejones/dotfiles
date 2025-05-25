975060915e2c5500ad0ec54e0cf13a831f4364f8
Kesse Jones <kjonesfc@outlook.com>
2025-05-24 21:23:10 -0300

    feat: show nix-shell badge in fish shell prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -3,23 +3,18 @@ function fish_prompt
     set -l git_info (string trim (fish_git_prompt) --chars " ()")
 
     echo -n (__fish_prompt_item --text="$cwd" --background=$fish_color_blue --foreground=$fish_color_base --reset=normal --bold)
+
+    if test -n "$IN_NIX_SHELL"
+        echo -n ' '
+        echo -n (__fish_prompt_item --text=" nix-shell" --background=$fish_color_lavender --foreground=$fish_color_base --reset=normal --bold)
+    end
+
     if test -n "$git_info"
         echo -n ' '
 
         echo -n (__fish_prompt_item --text=" $git_info" --background=$fish_color_mauve --foreground=$fish_color_base --reset=normal --bold)
     end
 
-    set -l mode_color (set_color normal)
-    switch $fish_bind_mode
-        case default
-            set mode_color (set_color $fish_color_red)
-        case insert
-            set mode_color (set_color $fish_color_subtext1)
-        case visual
-            set mode_color (set_color $fish_color_mauve)
-    end
-
-    echo -n "$mode_color"
     set_color normal
     echo -n -s ' '
 end
