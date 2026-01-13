12e2083fcdce52a8b6d5434e9d33be776372101a
Kesse Jones <kjonesfc@outlook.com>
2026-01-13 11:18:43 -0300

    feat: added keybindings for fuzzy git repos

--- .config/fish/functions/__fuzzy_git_repos.fish
+++ .config/fish/functions/__fuzzy_git_repos.fish
@@ -9,10 +9,21 @@ function __fuzzy_git_repos
         set -a fd_opts --search-path "Projects" 
     end
 
-    set -l sk_opts --preview "git -C $HOME/{} log -n 5 --pretty=medium --all --graph --color" \
+    set -l sk_opts \
+        --preview "git -C $HOME/{} log -n 5 --pretty=medium --all --graph --color" \
         --bind=ctrl-u:half-page-up,ctrl-d:half-page-down
 
-    set -l selected (fd $fd_opts | sk $sk_opts)
+    if test -n "$ZELLIJ"
+        set -l scripts_path "$HOME/.config/zellij/scripts"
+
+        set -a sk_opts \
+            --bind ctrl-e:execute-silent[$scripts_path/new-tab.sh {}] \
+            --bind ctrl-s:execute-silent[$scripts_path/new-pane.sh {} right] \
+            --bind ctrl-b:execute-silent[$scripts_path/new-pane.sh {} down] \
+            --bind ctrl-o:execute-silent[$scripts_path/new-pane.sh {} floating]
+    end
+
+    set -l selected (fd $fd_opts | sk $sk_opts --print0)
 
     if test -n "$selected"
         cd $HOME/$selected
---
+++ .config/zellij/scripts/fuzzy-tabs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SELECTED=(zellij action query-tab-names | sk --print0)
+
+if [ -z $SE ]; then
+fi
