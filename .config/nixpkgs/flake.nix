6fa0fd6df587a3c848292ee6f1595aa529f3de0d
Kesse Jones <kjonesfc@outlook.com>
2026-08-12 15:55:05 -0300

    chore: update config

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -2,7 +2,6 @@
   description = "Dotfiles for use with Nix flakes";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     zjstatus.url = "github:dj95/zjstatus";
     television.url = "github:alexpasmantier/television";
@@ -13,17 +12,13 @@
     zjstatus,
     television,
     nixpkgs,
-    flake-utils,
   }: let
-    modules = {
-      homeManagerModules.dotfiles = import ./modules/home-manager;
-      modules.dotfiles = import ./modules/nixos;
-    };
-  in
-    modules
-    // flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      overlays.default = import ./overlays {inherit zjstatus television pkgs;};
-    });
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeManagerModules.dotfiles = import ./modules/home-manager;
+    modules.dotfiles = import ./modules/nixos;
+
+    overlays.default = import ./overlays {inherit zjstatus television pkgs;};
+  };
 }
